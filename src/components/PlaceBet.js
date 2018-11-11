6b1b0746f7b24dbf0e90971fd8df428c12df4d2a
Andre Miras <andre.miras@gmail.com>
2018-11-11 23:33:52 +0100

    binds properties

--- src/components/PlaceBet.js
+++ src/components/PlaceBet.js
@@ -5,14 +5,31 @@ import 'rc-slider/assets/index.css';
 
 
 class ValueSlider extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+    };
+  }
+
+  onInputChange = (event) => {
+    this.setState({value: Number(event.target.value)});
+  }
+
+  onSliderChange = (value) => {
+    console.log(value);
+    this.setState({value});
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col">
-          <input className="form-control" />
+          <input type="number" className="form-control" onChange={this.onInputChange} value={this.state.value} />
         </div>
-        <div className="col-11">
-          <Slider />
+        <div className="col-10">
+          <Slider onChange={this.onSliderChange} value={this.state.value} />
         </div>
       </div>
     );
@@ -20,22 +37,38 @@ class ValueSlider extends React.Component {
 }
 
 class BetSize extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+    };
+  }
+
   render() {
     return (
     <div className="form-group">
       <label>Bet size</label>
-      <ValueSlider />
+      <ValueSlider value={this.state.value} />
     </div>
     );
   }
 }
 
 class ChanceOfWinning extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+    };
+  }
+
   render() {
     return (
     <div className="form-group">
       <label>Chance of winning</label>
-      <ValueSlider />
+      <ValueSlider value={this.state.value} />
     </div>
     );
   }
@@ -46,7 +79,7 @@ class RollUnder extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: null,
+      value: props.value,
     };
   }
 
@@ -57,7 +90,7 @@ class RollUnder extends React.Component {
         <h3>Roll under</h3>
       </div>
       <div className="col">
-        <h3 className="roll-under-value">{this.props.value}</h3>
+        <h3 className="roll-under-value">{this.state.value}</h3>
       </div>
     </div>
     );
@@ -77,9 +110,8 @@ class PlaceBet extends React.Component {
     return (
       <form className="PlaceBet">
         <h2>Place your bet</h2>
-        <BetSize />
-        <ChanceOfWinning />
-        {/* TODO */}
+        <BetSize value={1} mon={0} max={10} />
+        <ChanceOfWinning value={50} min={1} max={100} />
         <RollUnder value="51" />
         <RollButton />
       </form>
