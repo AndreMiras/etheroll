edeefce06a976db84356920c08afacd8e59ca833
Andre Miras <andre.miras@gmail.com>
2018-11-15 23:09:24 +0100

    Adds fontawesome icons

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -6,17 +6,17 @@ class Headers extends Component {
       <header>
         {/* Fixed navbari */}
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <a className="navbar-brand" href="/">Etheroll</a>
+          <a className="navbar-brand" href="/"><i className="fas fa-dice-d20"></i> Etheroll</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarCollapse">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/"><i className="fas fa-home"></i> Home <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="https://github.com/AndreMiras/etheroll">About</a>
+                <a className="nav-link" href="https://github.com/AndreMiras/etheroll"><i className="fab fa-github-alt"></i> About</a>
               </li>
             </ul>
           </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
