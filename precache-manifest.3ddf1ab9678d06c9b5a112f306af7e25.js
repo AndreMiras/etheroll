88db1bbc92967afdeb9ebbc1e872f8c18f9ae20a
Andre Miras <andre.miras@gmail.com>
2018-11-15 22:28:08 +0100

    Updates

---
+++ precache-manifest.3ddf1ab9678d06c9b5a112f306af7e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2c66284e4ae7522f310",
+    "url": "/etheroll/static/js/runtime~main.f2c66284.js"
+  },
+  {
+    "revision": "6f5b4d86b3b79a6c6f32",
+    "url": "/etheroll/static/js/main.6f5b4d86.chunk.js"
+  },
+  {
+    "revision": "e19628580b87dff4d18f",
+    "url": "/etheroll/static/js/1.e1962858.chunk.js"
+  },
+  {
+    "revision": "6f5b4d86b3b79a6c6f32",
+    "url": "/etheroll/static/css/main.19d13640.chunk.css"
+  },
+  {
+    "revision": "e19628580b87dff4d18f",
+    "url": "/etheroll/static/css/1.baaeaa29.chunk.css"
+  },
+  {
+    "revision": "44e4a1c8c06edaa36cd3015badc57136",
+    "url": "/etheroll/index.html"
+  }
+];
