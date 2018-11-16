bcfa2af5e6656fe562bc0423a3fb87ae38652009
Andre Miras <andre.miras@gmail.com>
2018-11-16 23:39:52 +0100

    PlaceBet.js linting

--- src/components/PlaceBet.js
+++ src/components/PlaceBet.js
@@ -2,116 +2,180 @@ import React from 'react';
 import './css/PlaceBet.css';
 import Slider from 'rc-slider/lib/Slider';
 import 'rc-slider/assets/index.css';
-import getWeb3 from "../utils/get-web3";
-import {getEtherollContractSync, etherscanUrls, Networks, contractAddresses} from "../utils/etheroll-contract";
+import PropTypes from 'prop-types';
+import getWeb3 from '../utils/get-web3';
+import {
+  getEtherollContractSync, etherscanUrls, Networks, contractAddresses,
+} from '../utils/etheroll-contract';
 
 
-function ValueSlider(props) {
+function ValueSlider({
+  value, updateValue, step, max,
+}) {
   return (
     <div className="row">
       <div className="col-sm-3 col-lg-2">
-        <input type="number" className="form-control"
-            onChange={(e) => props.updateValue(Number(e.target.value))} value={props.value} />
+        <input
+          type="number"
+          className="form-control"
+          onChange={e => updateValue(Number(e.target.value))}
+          value={value}
+        />
       </div>
       <div className="col">
-        <Slider onChange={props.updateValue} value={props.value} step={props.step} max={props.max} />
+        <Slider onChange={updateValue} value={value} step={step} max={max} />
       </div>
     </div>
   );
 }
+ValueSlider.propTypes = {
+  value: PropTypes.number.isRequired,
+  updateValue: PropTypes.func.isRequired,
+  step: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+};
 
-function BetSize(props) {
+function BetSize({ betSize, updateBetSize }) {
   return (
     <div className="form-group">
-      <label>Bet size</label>
-      <ValueSlider value={props.betSize} updateValue={props.updateBetSize} step={0.1} max={10} />
+      <b>Bet size</b>
+      <ValueSlider value={betSize} updateValue={updateBetSize} step={0.1} max={10} />
     </div>
   );
 }
+BetSize.propTypes = {
+  betSize: PropTypes.number.isRequired,
+  updateBetSize: PropTypes.func.isRequired,
+};
 
-function ChanceOfWinning(props) {
+function ChanceOfWinning({ chances, updateChances }) {
   return (
     <div className="form-group">
-      <label>Chance of winning</label>
-      <ValueSlider value={props.chances} updateValue={props.updateChances} />
+      <b>Chance of winning</b>
+      <ValueSlider value={chances} updateValue={updateChances} />
     </div>
   );
 }
+ChanceOfWinning.propTypes = {
+  chances: PropTypes.number.isRequired,
+  updateChances: PropTypes.func.isRequired,
+};
 
-function RollUnder(props) {
+function RollUnder({ value }) {
   return (
     <div className="row">
       <div className="col">
         <h3>Roll under</h3>
       </div>
       <div className="col">
-        <h3 className="roll-under-value">{props.value}</h3>
+        <h3 className="roll-under-value">{value}</h3>
       </div>
     </div>
   );
 }
+RollUnder.propTypes = {
+  value: PropTypes.number.isRequired,
+};
 
-// const Button = ({ text }) => (
-//     <button type="button" className="btn btn-primary btn-lg">{text}</button>
-// )
-
-function Button(props) {
-    return (
-      <button type="button" className="btn btn-primary btn-lg" onClick={props.onClick}>
-        {props.text}
-      </button>
-    );
-}
-
-function RollButton(props) {
-  return <Button text="Roll" onClick={props.onClick} />
-}
-
-function Transaction (props) {
-  const url = etherscanUrls[props.network] + '/tx/' + props.hash;
-  return <a href={url}>{props.hash}</a>
-}
-
-function Address (props) {
-  const url = etherscanUrls[props.network] + '/address/' + props.address;
-  return <a href={url}>{props.address}</a>
-}
-
-function Wallet (props) {
-  return <span>Wallet: <Address network={props.network} address={props.address} /></span>
-}
-
-function Contract (props) {
-  return <span>Contract: <Address network={props.network} address={props.address} /></span>
-}
-
-function ContractInfo (props) {
+function Button({ onClick, text }) {
   return (
-  <div className="row">
-    <div className="col">
-      <Contract network={props.network} address={props.contractAddress} />
-    </div>
-    <div className="col">
-      <Wallet network={props.network} address={props.account} />
-    </div>
-  </div>
+    <button type="button" className="btn btn-primary btn-lg" onClick={onClick}>
+      {text}
+    </button>
   );
 }
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
-function Transactions (props) {
-  if (props.transactions.length === 0) return <span />;
-  const transactions = props.transactions.map((item, index) => (
-    <li key={index} className="list-group-item"><Transaction network={props.network} hash={item} /></li>
+function RollButton({ onClick }) {
+  return <Button text="Roll" onClick={onClick} />;
+}
+RollButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+function Transaction({ hash, network }) {
+  const url = `${etherscanUrls[network]} /tx/ ${hash}`;
+  return <a href={url}>{hash}</a>;
+}
+Transaction.propTypes = {
+  hash: PropTypes.string.isRequired,
+  network: PropTypes.number.isRequired,
+};
+
+function Address({ address, network }) {
+  const url = `${etherscanUrls[network]} /address/ ${address}`;
+  return <a href={url}>{address}</a>;
+}
+Address.propTypes = {
+  address: PropTypes.string.isRequired,
+  network: PropTypes.number.isRequired,
+};
+
+function Wallet({ address, network }) {
+  return (
+    <span>
+      Wallet:
+      <Address address={address} network={network} />
+    </span>
+  );
+}
+Wallet.propTypes = Address.propTypes;
+
+function Contract({ address, network }) {
+  return (
+    <span>
+      Contract:
+      <Address address={address} network={network} />
+    </span>
+  );
+}
+Contract.propTypes = Address.propTypes;
+
+function ContractInfo({ account, contractAddress, network }) {
+  if (account === null) {
+    return <span>No account connected, connect with MetaMask.</span>;
+  }
+  return (
+    <div className="row">
+      <div className="col">
+        <Contract network={network} address={contractAddress} />
+      </div>
+      <div className="col">
+        <Wallet network={network} address={account} />
+      </div>
+    </div>
+  );
+}
+ContractInfo.propTypes = {
+  account: PropTypes.string,
+  contractAddress: PropTypes.string.isRequired,
+  network: PropTypes.number.isRequired,
+};
+ContractInfo.defaultProps = {
+  account: null,
+};
+
+function Transactions({ network, transactions }) {
+  if (transactions.length === 0) return <span />;
+  const transactionsElems = transactions.map(item => (
+    <li key={item} className="list-group-item"><Transaction network={network} hash={item} /></li>
   ));
   return (
     <div className="card transactions">
       <div className="card-header">Transactions</div>
       <div className="card-body">
-        <ul className="list-group">{transactions}</ul>
+        <ul className="list-group">{transactionsElems}</ul>
       </div>
     </div>
   );
 }
+Transactions.propTypes = {
+  network: PropTypes.number.isRequired,
+  transactions: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 class PlaceBet extends React.Component {
   constructor(props) {
@@ -122,7 +186,7 @@ class PlaceBet extends React.Component {
       account: null,
       web3: null,
       network: Networks.mainnet,
-      contract: contractAddresses[Networks.mainnet],
+      contractAddress: contractAddresses[Networks.mainnet],
       rollTransactions: [],
     };
   }
@@ -154,7 +218,7 @@ class PlaceBet extends React.Component {
       const contract = getEtherollContractSync(web3);
       this.setState({
         web3,
-        network: web3.version.network,
+        network: Number(web3.version.network),
         contract,
         contractAddress: contract.address,
       });
@@ -180,7 +244,7 @@ class PlaceBet extends React.Component {
     const rollUnder = chances + 1;
     return (
       <div>
-        <ContractInfo contractAddress={contractAddress} network={network} account={account} />
+        <ContractInfo account={account} contractAddress={contractAddress} network={network} />
         <form className="PlaceBet">
           <h2>Place your bet</h2>
           <BetSize betSize={betSize} updateBetSize={this.updateState('betSize')} />
