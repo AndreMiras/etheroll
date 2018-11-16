3cb018946afbb233006cb54c81531912c9f32dfc
Andre Miras <andre.miras@gmail.com>
2018-11-16 20:14:21 +0100

    Linter fixes on etheroll-contract.js and get-web3.js
    Not everything is covered yet.

--- src/utils/get-web3.js
+++ src/utils/get-web3.js
@@ -1,21 +1,21 @@
 import Web3 from 'web3';
 
-let getWeb3 = new Promise(function(resolve, reject) {
+const getWeb3 = new Promise((resolve, reject) => {
   // wait for loading completion before loading web3, to be sure it's
   // already injected
-  window.addEventListener('load', function() {
+  window.addEventListener('load', () => {
     let results;
     let web3js;
     // checking if Web3 has been injected by the browser MetaMask
     if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
       // Use MetaMask's provider.
       web3js = new Web3(window.web3.currentProvider);
-      results = {web3: web3js};
+      results = { web3: web3js };
       console.log('Injected web3 detected.');
       resolve(results);
     } else {
       // user is not running MetaMask?
-      console.log('Is MetaMask running?')
+      console.log('Is MetaMask running?');
     }
   });
 });
