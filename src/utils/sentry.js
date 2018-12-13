6b7238faaa08ea9ef4b384000df0673a2b6276b7
Andre Miras <andre.miras@gmail.com>
2018-12-13 14:50:17 +0100

    Enable Sentry only on production

--- src/utils/sentry.js
+++ src/utils/sentry.js
@@ -2,6 +2,9 @@ import * as Sentry from '@sentry/browser';
 import { version } from '../../package.json';
 
 function setupSentry() {
+  if (process.env.NODE_ENV !== 'production') {
+    return;
+  }
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
     release: version,
