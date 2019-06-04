91ca4589e8f620feea4296618eaf0c202a81d94c
Simon Boeuf <simon.boeuf@gmail.com>
2019-06-04 21:17:32 +0200

    refactor EtherollContract class

--- src/utils/etheroll-contract.js
+++ src/utils/etheroll-contract.js
@@ -21,7 +21,7 @@ function getProfit(betSize, winProbability) {
   }
   const rawPayout = getPayout(betSize, winProbability);
   const netPayout = cutHouseEdge(rawPayout);
-  
+
   return netPayout - betSize;
 }
 
@@ -36,52 +36,37 @@ function cutHouseEdge(payout) {
 }
 
 class EtherollContract {
-  constructor(web3, address) {
+  constructor(web3, address = contractAddresses[web3.version.network]) {
     this.web3 = web3;
     this.address = address;
-    if (typeof address === 'undefined') {
-      this.address = contractAddresses[web3.version.network];
-    }
     this.abi = etherollAbi;
     this.web3Contract = web3.eth.contract(etherollAbi).at(this.address);
   }
 
   getSolidityEvents() {
-    const events = {};
-    this.abi.forEach((definition) => {
-      if (definition.type !== 'event') {
-        return;
-      }
-      events[definition.name] = new SolidityEvent(this.web3, definition, this.address);
-    });
-    return events;
+    return this.abi
+      .filter(definition => definition.type === 'event')
+      .map(definition => [name, new SolidityEvent(this.web3, definition, this.address)]);
   }
 
   // Returns sha3 signature of events, e.g.
   // {'LogResult': '0x6883...5c88', 'LogBet': '0x1cb5...75c4'}
   getEventSignatures() {
-    const signatures = {};
     const events = this.getSolidityEvents();
-    Object.keys(events).forEach((eventName) => {
-      signatures[eventName] = events[eventName].signature();
-    });
-    return signatures;
+    return events.reduce((signatures, [name, value]) => ({...signatures, [name]: value}), {});
   }
 
   getSolidityEvent(eventSignature) {
     const events = this.getSolidityEvents();
-    const matchingEvent = Object.keys(events).filter(key => (
-      events[key].signature() === eventSignature.replace('0x', '')
-    ));
-    return events[matchingEvent];
+    const [_, matchingEvent] = events.find(([_, value]) => value.signature() === eventSignature.replace('0x', ''));
+    return matchingEvent;
   }
 
   decodeEvent(_evnt) {
     // SolidityEvent.decode() seems to be mutating the object, hence the copy
     const evnt = { ..._evnt };
     const solidityEvent = this.getSolidityEvent(evnt.topics[0]);
-    const decoded = solidityEvent.decode(evnt);
-    return decoded;
+    return solidityEvent.decode(evnt);
   }
 
   // callback(error, result)
@@ -90,16 +75,7 @@ class EtherollContract {
       if (error) {
         console.log(error);
       } else {
-        const { address } = this;
-        const lastBlock = result;
-        const fromBlock = lastBlock - 100;
-        const toBlock = lastBlock;
-        const options = {
-          address,
-          fromBlock,
-          toBlock,
-        };
-        const filter = this.web3.eth.filter(options);
+        const filter = this.getFilter(result);
         filter.get(callback);
       }
     });
@@ -111,32 +87,34 @@ class EtherollContract {
       if (error) {
         console.log(error);
       } else {
-        const { address } = this;
-        const lastBlock = result;
-        const fromBlock = lastBlock - 100;
-        const toBlock = lastBlock;
-        const options = {
-          address,
-          fromBlock,
-          toBlock,
-        };
-        const filter = this.web3.eth.filter(options);
+        const filter = this.getFilter(result);
         filter.watch(callback);
       }
     });
   }
 
+  getFilter(result) {
+    const { address } = this;
+    const toBlock = result;
+    const fromBlock = toBlock - 100;
+    const options = {
+      address,
+      fromBlock,
+      toBlock,
+    };
+    return this.web3.eth.filter(options);
+  }
+
   // Merges bet logs (LogBet) with bet results logs (LogResult).
   static mergeLogs(logBetEvents, logResultEvents) {
-    // per bet ID dictionary
-    const logResultEventsDict = logResultEvents.reduce((dict, logResultEvent) => ({
-      ...dict,
-      [logResultEvent.args.BetID]: logResultEvent,
-    }), {});
     return logBetEvents.map(logBetEvent => ({
       logBetEvent,
-      logResultEvent: logResultEventsDict[logBetEvent.args.BetID],
+      logResultEvent: findLogResultEventBylogBetEvent(logBetEvent),
     }));
+
+    function findLogResultEventBylogBetEvent(logBetEvent) {
+      return logResultEvents.find(logResultEvent => logResultEvent.args.BetID === logBetEvent.args.BetID);
+    }
   }
 
   // callback(error, result)
@@ -146,12 +124,8 @@ class EtherollContract {
         console.log(error);
       } else {
         const decodedEvents = result.map(evnt => this.decodeEvent(evnt));
-        const logBetEvents = decodedEvents.filter(evnt => (
-          evnt.event === 'LogBet'
-        ));
-        const logResultEvents = decodedEvents.filter(evnt => (
-          evnt.event === 'LogResult'
-        ));
+        const logBetEvents = decodedEvents.filter(evnt => evnt.event === 'LogBet');
+        const logResultEvents = decodedEvents.filter(evnt => evnt.event === 'LogResult');
         const mergedLogs = EtherollContract.mergeLogs(logBetEvents, logResultEvents);
         callback(error, mergedLogs);
       }
