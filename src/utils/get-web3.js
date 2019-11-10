ebc58d6dc845afe8e7fa579fa5c28e5eae1f861f
Andre Miras <andre.miras@gmail.com>
2019-11-10 01:40:15 +0100

    Request account access (for placing bets)
    Note this is not mandatory to see roll history.

--- src/utils/get-web3.js
+++ src/utils/get-web3.js
@@ -5,8 +5,12 @@ const getWeb3 = new Promise((resolve, reject) => {
     let web3;
     if (window.ethereum) { // Modern dapp browsers...
       web3 = new Web3(window.ethereum);
-      // Request account access if needed
-      // await window.ethereum.enable();
+      try {
+        // Request account access if needed
+        await window.ethereum.enable();
+      } catch(error) {
+        console.error(error);
+      }
     } else if (window.web3) { // Legacy dapp browsers...
       web3 = new Web3(window.web3.currentProvider);
     } else { // Non-dapp browsers...
